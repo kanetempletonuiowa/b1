38a8caf82f8714ac5a179ab756acc81c59552e28
kanetempletonuiowa <kane-templeton@uiowa.edu>
2016-11-04 09:07:25 -0500

    initial basic commit
    basic files to get started with. will keep updating them throughout the day to make them more functional

---
+++ warehouse/src/warehouse/event/Event.java
@@ -0,0 +1,14 @@
+package warehouse.event;
+
+public class Event {
+	
+	private int fireTime;
+	
+	public Event(int t) {
+		fireTime=t;
+	}
+	
+	public int getFireTime() {return fireTime;}
+	public void setFireTime(int t){fireTime=t;}
+
+}
---
+++ warehouse/src/warehouse/event/Task.java
@@ -0,0 +1,7 @@
+package warehouse.event;
+
+public interface Task {
+	
+	public void fire(String arg);
+
+}
