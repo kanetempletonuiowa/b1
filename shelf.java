aad9b8620d6c96f03f10232a69a239cd671b6831
ppphb1202 <hanbo-pen@uiowa.edu>
2016-11-07 21:25:08 -0600

    Add files via upload

---
+++ shelf.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hanbo
+ */
+public class shelf extends inventory{
+    int shelfnum;
+    String itemname;
+    
+    public void initialshelf(int x){
+        x = 1;
+        this.shelfnum = x;
+    }
+
+
+}
